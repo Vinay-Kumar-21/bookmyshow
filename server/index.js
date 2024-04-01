c6ad1cfed80f996cbe0fd9bc5d067b3d935a5bcb
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-04-01 07:56:00 +0530

    servr setup done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+require("dotenv").config();
+require("./config/dbconfig");
+
 
 app.listen(8082, () => {
     console.log("Server started at 8082");
