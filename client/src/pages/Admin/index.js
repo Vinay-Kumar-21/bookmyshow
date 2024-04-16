cc646fe7355fca7470fd3d83c104689e0b8af0c0
vinay-kumar-21 <vinaykumarraju21@gmail.com>
2024-04-16 21:38:50 +0530

    almost same

--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,25 +1,26 @@
-import { Tabs } from 'antd'
-import React from 'react'
-
+import React from "react";
+import { Tabs } from "antd";
 import PageTitle from "../../components/PageTitle";
-import MoviesList from './MoviesList';
-import TheatresList from "./TheatresList";
+ import MoviesList from "./MoviesList";
+ import TheatresList from "./TheatresList";
+//  import UpcomingList from './UpcomingList'
 
 function Admin() {
-    return (
-        <div>
-            <PageTitle title="Admin" />
-            <Tabs defaultActiveKey='movies'>
-                <Tabs.TabPane tab="Movies" key="movies">
-                    <MoviesList />
-                </Tabs.TabPane>
+  return (
+    <div>
+      <PageTitle title="Admin" />
 
-                <Tabs.TabPane tab="Theatres" key="theatres">
-                    <TheatresList/>
-                </Tabs.TabPane>
-            </Tabs>
-        </div>
-    )
+      <Tabs defaultActiveKey="movies">
+        <Tabs.TabPane tab="Movies" key="movies">
+          <MoviesList />
+        </Tabs.TabPane>
+
+        <Tabs.TabPane tab="Theatres" key="theatres">
+          <TheatresList />
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  );
 }
 
-export default Admin
+export default Admin;
